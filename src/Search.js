2c51afb6d54a8c42ff820ae7b7edb5324138a672
Sera <74925057+Sera-iso@users.noreply.github.com>
2021-06-05 21:43:55 +0200

    Created Results.js component to display the API response results

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Search.css";
 
 export default function Search() {
@@ -11,7 +12,7 @@ export default function Search() {
 
     function handleSearch(event){
         event.preventDefault();
-        
+
         // documentation on https://dictionaryapi.dev/
         let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
@@ -26,6 +27,7 @@ export default function Search() {
             <form onSubmit={handleSearch}>
                 <input type="search" onChange={handleKeyword} />
             </form>
+            <Results />
         </div>
     )
 }
