e19917702ee5abb5c605708bb3bfaa148b29b725
Sera <74925057+Sera-iso@users.noreply.github.com>
2021-06-05 21:06:49 +0200

    Added Search functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <p>
           This will become a pretty Dictionary app built in React
         </p>
--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+    let [keyword, setKeyword] = useState("");
+    function handleSearch(event){
+        event.preventDefault();
+        alert(`Searching for ${keyword}`);
+    }
+    function handleKeyword(event) {
+        setKeyword(event.target.value);
+    }
+
     return (
         <div className="Search">
-            Hello
+            <form onSubmit={handleSearch}>
+                <input type="search" onChange={handleKeyword} />
+            </form>
         </div>
     )
 }
