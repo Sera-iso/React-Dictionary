9b3a04214423e972dbbfedd251a40b4730ca3308
Sera <74925057+Sera-iso@users.noreply.github.com>
2021-08-01 16:08:56 +0200

    Styled the .Phonetics component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,8 +5,7 @@ export default function Phonetics(props) {
     return (
         <div className="Phonetics">
             <a href={props.phonetics.audio} target="_blank" rel="noreferrer">Listen</a>
-            <br />
-            {props.phonetics.text}
+            <span className="text">{props.phonetics.text}</span>
         </div>
     )
 }
