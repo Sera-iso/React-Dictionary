5671b9734394e7c0dd28b0feae90a65d195c117a
Sera <74925057+Sera-iso@users.noreply.github.com>
2021-08-01 17:45:31 +0200

    Implemented API for images to match with searched words

--- src/Search.js
+++ src/Search.js
@@ -7,16 +7,26 @@ export default function Search() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data[0]);
+    }
+
+    function handleImagesResponse(response) {
+        console.log(response);
     }
 
     function handleSearch(event){
         event.preventDefault();
 
-        // documentation on https://dictionaryapi.dev/
-        let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        // Dictionary API documentation https://dictionaryapi.dev/
+        let dictionaryApiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(dictionaryApiUrl).then(handleDictionaryResponse);
+
+        // Images API documentation https://www.pexels.com/api/documentation/
+        let imagesApiKey= '563492ad6f91700001000001291af85a5f9740ac9f25fa533940d361';
+        let header = { Authorization: `Bearer ${imagesApiKey}` };
+        let imagesApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+        axios.get(imagesApiUrl, { headers: header } ).then(handleImagesResponse);
     }
 
     function handleKeyword(event) {
